36179f5266dfcafdd0297ad0d447e5e71c0893a0
0jas0jas <ojas1807@gmail.com>
2024-10-15 02:51:02 -0400

    Added recommended function thingy

--- blendtube/api/recommend.py
+++ blendtube/api/recommend.py
@@ -1,2 +1,17 @@
 from youtubesearchpython import VideosSearch
 
+def recommendURLs(urls: list) -> list:
+    return_lst = []
+    
+    for url in urls:
+        videosSearch = VideosSearch(url, limit=2)
+        results = videosSearch.result()['result']
+        
+        search_result_urls = []
+        
+        for result in results:
+            search_result_urls.append(result['link'])
+        
+        return_lst.append(search_result_urls)
+    
+    return return_lst
